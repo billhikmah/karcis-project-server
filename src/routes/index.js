3e508a6470b5856c48c5bad8f34ad570078190d9
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-09-02 02:13:49 +0700

    Add new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 
 const Router = express.Router();
+const authRouter = require("./auth");
+const eventRouter = require("./event");
+const transactionRouter = require("./transaction");
+const userRouter = require("./user");
+const wishlistRouter = require("./wishlist");
 
 Router.get("/ping", (request, response) => {
   response.status(200).send("Hello World!");
 });
 
 module.exports = Router;
+
+Router.use("/auth", authRouter);
+Router.use("/event", eventRouter);
+Router.use("/transactions", transactionRouter);
+Router.use("/users", userRouter);
+Router.use("/wishlist", wishlistRouter);
