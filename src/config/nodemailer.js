b4d9d60506a4633dc26b05eae98691c1f6a36211
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-09-04 18:52:55 +0700

    Add nodemailer

---
+++ src/config/nodemailer.js
@@ -0,0 +1,48 @@
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const handlebars = require("handlebars");
+const fs = require("fs");
+const { promisify } = require("util");
+require("dotenv").config();
+
+const readFile = promisify(fs.readFile);
+
+const createToken = (id, email) => {
+  const userPayload = { email, id };
+  const jwtOptions = { issuer: process.env.JWT_ISSUER, expiresIn: "600s" };
+  const token = jwt.sign(userPayload, process.env.JWT_KEY, jwtOptions);
+  return token;
+};
+
+const sendConfirmationEmail = async (id, name, email) => {
+  const token = createToken(id, email);
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    },
+  });
+  const html = await readFile("./src/templates/confirmation.html", "utf8");
+  const template = handlebars.compile(html);
+  const data = {
+    url: `${process.env.CLIENT_URL}confirmation/${token}`,
+    name: name.toUpperCase(),
+  };
+  const htmlToSend = template(data);
+
+  const mailOptions = {
+    from: process.env.MAIL_SENDER,
+    to: email,
+    subject: `Karcis - Activate Your Account`,
+    html: htmlToSend,
+  };
+
+  transporter.sendMail(mailOptions);
+};
+
+module.exports = { sendConfirmationEmail };
