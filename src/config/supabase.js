5f62ac3c66887e076478c71625d1477b4f332070
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-09-04 18:53:02 +0700

    Update

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,8 +1,8 @@
 const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
 
-const supabaseUrl = "https://aozqughyxueicvaduidp.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvenF1Z2h5eHVlaWN2YWR1aWRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjIwODc4NjUsImV4cCI6MTk3NzY2Mzg2NX0.n91XEN-xmQgr5yh4LydJWoDM28R8C5fw-KqlqazofAc";
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
