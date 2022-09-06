ce254cbb2a0ded7814a810b57338f0e1824b020e
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-09-06 20:33:20 +0700

    Add create and get booking and booking section

--- src/models/booking.js
+++ src/models/booking.js
@@ -4,14 +4,9 @@ const supabase = require("../config/supabase");
 const createBooking = (params, body) =>
   new Promise((resolve, reject) => {
     const { user_id } = params;
-    const {
-      event_id,
-      total_ticket,
-      total_payment,
-      payment_method,
-      payment_status,
-    } = body;
-
+    const { event_id, total_payment, payment_method, payment_status, section } =
+      body;
+    const total_ticket = section.length;
     supabase
       .from("booking")
       .insert([
@@ -33,4 +28,43 @@ const createBooking = (params, body) =>
       });
   });
 
-module.exports = { createBooking };
+const createBookingSection = (booking_id, section, status_used) =>
+  new Promise((resolve, reject) => {
+    supabase
+      .from("booking_section")
+      .insert([
+        {
+          booking_id,
+          section,
+          status_used,
+        },
+      ])
+      .then((result) => {
+        if (!result.error) {
+          resolve(result);
+        } else {
+          reject(result);
+        }
+      });
+  });
+
+const getBookingByUserId = (user_id) =>
+  new Promise((resolve, reject) => {
+    supabase
+      .from("booking")
+      .select(`*, booking_section(*), user_id(id, name), event_id(id, name)`)
+      .match({ user_id })
+      .then((result) => {
+        if (!result.error) {
+          resolve(result);
+        } else {
+          reject(result);
+        }
+      });
+  });
+
+module.exports = {
+  createBooking,
+  createBookingSection,
+  getBookingByUserId,
+};
--- src/routes/booking.js
+++ src/routes/booking.js
@@ -4,5 +4,6 @@ const bookingController = require("../controllers/booking");
 const Router = express.Router();
 
 Router.post("/:user_id", bookingController.createBooking);
+Router.get("/:user_id", bookingController.getBookingByUserId);
 
 module.exports = Router;
