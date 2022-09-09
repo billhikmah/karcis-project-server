ae849fc736da6ddeacadf7c1cfb3183380077bb8
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-09-10 00:28:20 +0700

    Add authentication and authorization

--- src/routes/event.js
+++ src/routes/event.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const eventController = require("../controllers/event");
+const authMiddleware = require("../middlewares/auth");
 
 const Router = express.Router();
 
-Router.post("/", eventController.createEvent);
+Router.post(
+  "/",
+  authMiddleware.authentication,
+  authMiddleware.adminAuthorization,
+  eventController.createEvent
+);
 Router.get("/", eventController.getAllEvents);
 Router.get("/:id", eventController.getEventById);
-Router.patch("/:id", eventController.updateEvent);
-Router.delete("/:id", eventController.deleteEvent);
+Router.patch(
+  "/:id",
+  authMiddleware.authentication,
+  authMiddleware.adminAuthorization,
+  eventController.updateEvent
+);
+Router.delete(
+  "/:id",
+  authMiddleware.authentication,
+  authMiddleware.adminAuthorization,
+  eventController.deleteEvent
+);
 
 module.exports = Router;
--- src/routes/wishlist.js
+++ src/routes/wishlist.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const wishlistController = require("../controllers/wishlist");
+const authMiddleware = require("../middlewares/auth");
 
 const Router = express.Router();
 
-Router.post("/", wishlistController.createWishlist);
+Router.post(
+  "/",
+  authMiddleware.authentication,
+  wishlistController.createWishlist
+);
 Router.get("/:user_id", wishlistController.getAllWishList);
 Router.get("/id/:id", wishlistController.getWishlistById);
 Router.delete("/:id", wishlistController.deleteWishlist);
