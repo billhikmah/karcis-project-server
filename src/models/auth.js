1cee004ad3634722b2020f12556243944687fce5
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-09-10 00:31:37 +0700

    Add check registered email

--- src/models/auth.js
+++ src/models/auth.js
@@ -14,4 +14,48 @@ const signUp = (name, email, password) =>
       });
   });
 
-module.exports = { signUp };
+const checkRegisteredEmail = (email) =>
+  new Promise((resolve, reject) => {
+    supabase
+      .from("user")
+      .select("email")
+      .eq("email", email)
+      .then((result) => {
+        if (!result.error) {
+          resolve(result);
+        } else {
+          reject(result);
+        }
+      });
+  });
+
+const logIn = (email, password) =>
+  new Promise((resolve, reject) => {
+    supabase
+      .from("user")
+      .insert([{ email, password }])
+      .then((result) => {
+        if (!result.error) {
+          resolve(result);
+        } else {
+          reject(result);
+        }
+      });
+  });
+
+const getUserByEmail = (email) =>
+  new Promise((resolve, reject) => {
+    supabase
+      .from("user")
+      .select("password, id, role")
+      .eq("email", email)
+      .then((result) => {
+        if (!result.error) {
+          resolve(result);
+        } else {
+          reject(result);
+        }
+      });
+  });
+
+module.exports = { signUp, checkRegisteredEmail, logIn, getUserByEmail };
