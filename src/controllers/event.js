60228a4981b4338c48e97a19e571a9ebca7e426a
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-09-10 17:26:25 +0700

    Update some response messages

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -4,7 +4,7 @@ const responseHandler = require("../utils/responseHandler");
 const createEvent = async (req, res) => {
   try {
     const result = await eventModel.createEvent(req.body);
-    responseHandler(res, result.status, result.statusText, result.data);
+    responseHandler(res, result.status, "Event has been created", result.data);
   } catch (error) {
     responseHandler(res, error.status, error.error.message);
   }
@@ -14,7 +14,7 @@ const getEventById = async (req, res) => {
   try {
     const result = await eventModel.getEventById(req.params.id);
     if (result.data.length === 0) {
-      return responseHandler(res, 404, "Data not found.", result.data);
+      return responseHandler(res, 404, "Event not found.", result.data);
     }
     return responseHandler(res, result.status, result.statusText, result.data);
   } catch (error) {
@@ -70,7 +70,12 @@ const deleteEvent = async (req, res) => {
   try {
     const result = await eventModel.deleteEvent(req.params);
     if (result.data.length === 0) {
-      return responseHandler(res, result.status, "Id not found", result.data);
+      return responseHandler(
+        res,
+        result.status,
+        "Event not found",
+        result.data
+      );
     }
     return responseHandler(
       res,
