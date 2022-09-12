e0fafa195593d1f6b505702020da30e71515093a
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-09-13 02:07:50 +0700

    Add update user image

--- src/middlewares/imageDestroyer.js
+++ src/middlewares/imageDestroyer.js
@@ -1,6 +1,8 @@
+/* eslint-disable camelcase */
 const responseHandler = require("../utils/responseHandler");
 const cloudinary = require("../config/cloudinary");
 const { getEventById } = require("../models/event");
+const { getUserById } = require("../models/user");
 
 const updateEventImage = async (req, res, next) => {
   try {
@@ -49,4 +51,29 @@ const deleteEventImage = async (req, res, next) => {
   }
 };
 
-module.exports = { updateEventImage, deleteEventImage };
+const updateUserImage = async (req, res, next) => {
+  try {
+    if (req.file) {
+      const { user_id } = req.payload;
+      const getPublicId = await getUserById(user_id);
+      if (getPublicId.data.length === 0) {
+        return responseHandler(res, 404, "Event not found.", getPublicId.data);
+      }
+      // eslint-disable-next-line camelcase
+      const public_id = getPublicId.data[0].image || null;
+      return cloudinary.uploader
+        .destroy(public_id)
+        .then(() => {
+          next();
+        })
+        .catch(() => {
+          next();
+        });
+    }
+    return next();
+  } catch (error) {
+    return responseHandler(res, 404, error, null);
+  }
+};
+
+module.exports = { updateEventImage, deleteEventImage, updateUserImage };
