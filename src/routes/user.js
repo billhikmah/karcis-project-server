2f6e46ab2b2a2cc15913bf5e3546d17ad4358d7d
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-09-13 02:07:05 +0700

    Add authentication, authorization, image uploader, and image destroyer

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const userController = require("../controllers/user");
+const authMiddleware = require("../middlewares/auth");
+const imageDestroyer = require("../middlewares/imageDestroyer");
+const imageUploader = require("../middlewares/imageUploader");
 
 const Router = express.Router();
 
 Router.post("/", userController.createUser);
-Router.get("/", userController.getAllUser);
-Router.get("/:id", userController.getUserById);
-Router.patch("/:id", userController.updateUser);
+Router.get(
+  "/",
+  authMiddleware.authentication,
+  authMiddleware.adminAuthorization,
+  userController.getAllUser
+);
+Router.get(
+  "/details",
+  authMiddleware.authentication,
+  userController.getUserById
+);
+Router.patch(
+  "/",
+  authMiddleware.authentication,
+  imageUploader.uploadUserImage,
+  imageDestroyer.updateUserImage,
+  userController.updateUser
+);
 
 module.exports = Router;
