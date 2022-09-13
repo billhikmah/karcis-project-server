3ac1121294ddb6dab58a6ee9121fb74c0bc24b16
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-09-14 02:51:15 +0700

    Upload redis config

---
+++ src/config/redis.js
@@ -0,0 +1,15 @@
+const redis = require("redis");
+
+const client = redis.createClient({
+  url: `redis://${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+});
+
+(async () => {
+  client.connect();
+  client.on("connect", () => {
+    // eslint-disable-next-line no-console
+    console.log("You're connected to redis");
+  });
+})();
+
+module.exports = client;
