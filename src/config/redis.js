807e7c459aad90b1429f6efb95e8ccc1688e9d2c
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-09-15 01:09:27 +0700

    Update redis config

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,15 +1,18 @@
-const redis = require("redis");
+/* eslint-disable no-console */
+const { createClient } = require("redis");
 
-const client = redis.createClient({
-  url: `redis://${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+const client = createClient({
+  url: process.env.REDIS_CLIENT_URL,
 });
+const redisConn = async () => {
+  try {
+    client.on("error", (error) => console.log(error));
+    await client.connect();
 
-(async () => {
-  client.connect();
-  client.on("connect", () => {
-    // eslint-disable-next-line no-console
-    console.log("You're connected to redis");
-  });
-})();
+    console.log("You are connected to redis");
+  } catch (error) {
+    console.log(`Error:${error.message}`);
+  }
+};
 
-module.exports = client;
+module.exports = { redisConn, client };
