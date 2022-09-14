f5e44ad4f2f17e50890c6ca3010af8648100e710
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-09-15 03:59:55 +0700

    Updated and manipulated data of get booking section by id

--- src/controllers/booking.js
+++ src/controllers/booking.js
@@ -50,9 +50,48 @@ const getBookingSectionByEventId = async (req, res) => {
     if (result.data.length === 0) {
       return responseHandler(res, 404, "Booking not found.", result.data);
     }
-    return responseHandler(res, result.status, result.statusText, result.data);
+
+    const sectionList = [];
+    result.data.forEach((element) => {
+      element.booking_section.forEach((sectionElements) => {
+        sectionList.push(sectionElements.section);
+      });
+    });
+
+    const countedSection = sectionList.reduce((container, section) => {
+      const currentCount = container[section] || 0;
+      return {
+        ...container,
+        [section]: currentCount + 1,
+      };
+    }, {});
+
+    const capacity = [
+      { section: "VVIP", capacity: 10 },
+      { section: "VIP", capacity: 20 },
+      { section: "REG", capacity: 30 },
+    ];
+
+    const data = [];
+    // Get section name from countedSection
+    Object.keys(countedSection).forEach((sectionName) => {
+      // Get section capacity
+      const sectionCapacity = capacity.filter((sectionKey) =>
+        sectionName.includes(sectionKey.section)
+      );
+
+      const resultSection = {
+        section: sectionName,
+        booked: countedSection[sectionName],
+        available: sectionCapacity[0].capacity - countedSection[sectionName],
+        statusFull: sectionCapacity[0].capacity === countedSection[sectionName],
+      };
+      data.push(resultSection);
+    });
+
+    return responseHandler(res, result.status, result.statusText, data);
   } catch (error) {
-    return responseHandler(res, error.status, error.error.message);
+    return responseHandler(res, error.status, error.error.message, null);
   }
 };
 
