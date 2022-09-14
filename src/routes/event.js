d0b6bc89e4e78dc20bedf8d46a4f227842852c56
Bill Hikmah <billhikmah1@yahoo.co.id>
2022-09-15 01:52:17 +0700

    Add redis middlewares to some routes

--- src/routes/event.js
+++ src/routes/event.js
@@ -3,6 +3,7 @@ const eventController = require("../controllers/event");
 const authMiddleware = require("../middlewares/auth");
 const imageDestroyer = require("../middlewares/imageDestroyer");
 const imageUploader = require("../middlewares/imageUploader");
+const redisMiddleware = require("../middlewares/redis");
 
 const Router = express.Router();
 
@@ -11,16 +12,18 @@ Router.post(
   authMiddleware.authentication,
   authMiddleware.adminAuthorization,
   imageUploader.uploadEventImage,
+  redisMiddleware.clearEventsOnRedis,
   eventController.createEvent
 );
-Router.get("/", eventController.getAllEvents);
-Router.get("/:id", eventController.getEventById);
+Router.get("/", redisMiddleware.getAllEvents, eventController.getAllEvents);
+Router.get("/:id", redisMiddleware.getEventById, eventController.getEventById);
 Router.patch(
   "/:id",
   authMiddleware.authentication,
   authMiddleware.adminAuthorization,
   imageUploader.uploadEventImage,
   imageDestroyer.updateEventImage,
+  redisMiddleware.clearEventsOnRedis,
   eventController.updateEvent
 );
 Router.delete(
@@ -28,6 +31,7 @@ Router.delete(
   authMiddleware.authentication,
   authMiddleware.adminAuthorization,
   imageDestroyer.deleteEventImage,
+  redisMiddleware.clearEventsOnRedis,
   eventController.deleteEvent
 );
 
